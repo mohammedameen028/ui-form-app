ceea19bbd294842632ef64a25874d03ff7a7000f
mohammedameen028 <mohammedameen028@gmail.com>
2022-06-02 16:19:08 +0530

    adding some more functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,57 +1,74 @@
-import React from 'react';
+import React, { useState } from "react";
 import useForm from "./useForm";
-import validate from './FormValidationRules';
+import validate from "./FormValidationRules";
 
 const Form = () => {
-  const {
-    values,
-    errors,
-    formData,
-    handleChange,
-    handleSubmit,
-    handleReset,
-  } = useForm(submit, validate);
+  const { values, errors, formData, handleChange, handleSubmit, handleReset } =
+    useForm(submit, validate);
 
- 
+  const [formState, setFormState] = useState([]);
+
   function submit() {
-    console.log('No errors, submit callback called!', values);
+    console.log("No errors, submit callback called!", values);
+    setFormState((prevState) => [...prevState, values]);
   }
-  
+
   return (
     <div className="section is-fullheight">
       <div className="container">
         <div className="column is-4 is-offset-4">
           <div className="box">
             <form onSubmit={handleSubmit} noValidate>
-                {formData.map((item) => {
-                    return (
-                      <div className="field" key={item.id}>
-                        <label className="label">{item.label}</label>
-                        <div className="control">
-                          <input
-                            autoComplete="off"
-                            className={`input ${errors[item.field] && "is-danger"}`}
-                            type={item.type}
-                            name={item.field}
-                            onChange={handleChange}
-                            value={values[item.field]|| ""}
-                            required
-                          />
-                          {errors[item.field] && (
-                            <p className="help is-danger">{errors[item.field]}</p>
-                          )}
-                        </div>
-                      </div>
-                    );
-                })}
-                <div className="field is-grouped">
+              {formData.map((item) => {
+                return (
+                  <div className="field" key={item.id}>
+                    <label className="label">{item.label}</label>
                     <div className="control">
-                        <button type="submit" className="button is-info">Submit</button>
+                      <input
+                        autoComplete="off"
+                        className={`input ${errors[item.field] && "is-danger"}`}
+                        type={item.type}
+                        name={item.field}
+                        onChange={handleChange}
+                        value={values[item.field] || ""}
+                        required
+                      />
+                      {errors[item.field] && (
+                        <p className="help is-danger">{errors[item.field]}</p>
+                      )}
                     </div>
-                    <div className="control">
-                        <button type="reset" onClick={handleReset} className="button is-info is-outlined">Reset</button>
-                    </div>
+                  </div>
+                );
+              })}
+              <div className="field is-grouped">
+                <div className="control">
+                  <button type="submit" className="button is-info">
+                    Submit
+                  </button>
                 </div>
+                <div className="control">
+                  <button
+                    type="reset"
+                    onClick={handleReset}
+                    className="button is-info is-outlined"
+                  >
+                    Reset
+                  </button>
+                </div>
+              </div>
+              <div>
+                <div>
+                  {formState.map((item) => {
+                    return Object.entries(item).map(([name, value], index) => {
+                      return (
+                        <div key={index}>
+                          {name}: {value}
+                        </div>
+                      );
+                    });
+                  })}
+                </div>
+              </div>
             </form>
           </div>
         </div>
